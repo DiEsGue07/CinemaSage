52fdf8e3a196fdaf781b8a47e907a75f44442a61
DiEsGue07 <83434897+DiEsGue07@users.noreply.github.com>
2023-07-13 23:36:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error
@@ -13,20 +13,24 @@ print("Cargado")
 
 mlb = MultiLabelBinarizer()
 genre_encoded = mlb.fit_transform(movies_df['genre'].str.split('|'))
+
 genre_df = pd.DataFrame(genre_encoded, columns=mlb.classes_)
+
 movies_df = pd.concat([movies_df, genre_df], axis=1)
 
-print("Procesado")
+print("Preprocesado")
 
 features = mlb.classes_
 X = movies_df[features]
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-kmeans = KMeans(n_clusters=5, random_state=42)
-clusters = kmeans.fit_predict(X_scaled)
+dbscan = DBSCAN(eps=0.5, min_samples=5)
+clusters = dbscan.fit_predict(X_scaled)
+
+num_clusters = len(set(clusters)) - (1 if -1 in clusters else 0)
+print(f"Número de clusters creados: {num_clusters}")
 
 print("Clusterizado")
-
 merged_df = pd.merge(ratings_df, movies_df, on='movieId')
 X_train, X_test, y_train, y_test = train_test_split(merged_df[features], merged_df['rating'], test_size=0.2, random_state=42)
 
@@ -42,14 +46,17 @@ print(f'Error cuadrático medio (MSE): {mse}')
 def recommend_movies(genre):
     if genre in mlb.classes_:
         genre_movies = movies_df[movies_df[genre] == 1]
+
         user_movie_features = genre_movies[features]
         user_movie_ratings = model.predict(user_movie_features)
+
         recommendations = genre_movies.assign(rating=user_movie_ratings).sort_values('rating', ascending=False)
 
-        recommended_movies = recommendations['title'].head(5).to_string(index=False)
+        recommended_movies = recommendations['title'].head(10).to_string(index=False)
         print(f"Las siguientes películas del género '{genre}' te podrían interesar:\n{recommended_movies}")
     else:
         print("No se encontraron películas con el género especificado.")
+
 
 while True:
     genre = input("Ingresa el género de película (o escribe 'stop' para salir): ")
